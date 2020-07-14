2b839545ece487145d068ff52ca4a49d0732f60b
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2020-07-14 15:29:10 +0900

    コメント内容の修正を行いました。

--- db/db-challenge2/chatwork.sql
+++ db/db-challenge2/chatwork.sql
@@ -3,30 +3,30 @@ CREATE TABLE `users` (
     `user_name` VARCHAR(100) NOT NULL,
     `mail` VARCHAR(100) NOT NULL,
     `introduction` VARCHAR(1000),
-    `work_phone_number` INT(13),
-    `my_phone_number` INT(13),
+    `work_phone_number` VARCHAR(13),
+    `my_phone_number` VARCHAR(13),
     `is_deleted` TINYINT(1) NOT NULL DEFAULT 0,
-    `created_at` DATETIME NOT NULL ,
-    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP ,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE `chat_rooms` (
     `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
     `room_name` VARCHAR(100) NOT NULL,
     `description` VARCHAR(1000),
-    `is_sent_file` TINYINT(1) NOT NULL DEFAULT 0,
+    `can_sent_file` TINYINT(1) NOT NULL DEFAULT 0,
     `is_direct_chat` TINYINT(1) NOT NULL DEFAULT 0,
     `is_deleted` TINYINT(1) NOT NULL DEFAULT 0,
     `create_user_id` INT(11) NOT NULL REFERENCES users(`id`),
-    `create_at` DATETIME NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP ,
     `update_user_id` INT(11) NOT NULL REFERENCES users(`id`),
-    `update_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `update_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE `rooms_join_users` (
     `user_id` INT(11) REFERENCES users(`id`),
     `room_id` INT(11) REFERENCES char_rooms(`id`),
-    `uset_joined_at` DATETIME NOT NULL,
+    `user_joined_at` DATETIME NOT NULL,
     PRIMARY KEY (`user_id`,`room_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -37,20 +37,20 @@ CREATE TABLE `chat_posts` (
     `file_name` VARCHAR(100),
     `is_deleted` TINYINT(1) NOT NULL DEFAULT 0,
     `create_user_id` INT(11) NOT NULL REFERENCES users(`id`),
-    `create_at` DATETIME NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP ,
     `update_user_id` INT(11) NOT NULL REFERENCES users(`id`),
-    `update_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `update_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE `tasks` (
     `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
-    `task_user_id` INT(11) NOT NULL REFERENCES users(`id`),
+    `user_id` INT(11) NOT NULL REFERENCES users(`id`),
     `chat_room_id` INT(11) NOT NULL REFERENCES chat_rooms(`id`),
     `task_deadline` DATETIME,
     `is_completed` TINYINT(1) NOT NULL DEFAULT 0,
     `is_deleted` TINYINT(1) NOT NULL DEFAULT 0,
     `create_user_id` INT(11) NOT NULL REFERENCES users(`id`),
-    `create_at` DATETIME NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP ,
     `update_user_id` INT(11) NOT NULL REFERENCES users(`id`),
-    `update_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `update_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
