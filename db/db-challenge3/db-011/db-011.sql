9d63e9889b92834deb42728c8d289a6a77a7a35f
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2020-07-20 13:59:02 +0900

    レビューいただいた箇所の修正を行いました。

--- db/db-challenge3/db-011/db-011.sql
+++ db/db-challenge3/db-011/db-011.sql
@@ -1,5 +1,5 @@
 BEGIN;
 UPDATE `tasks`
 SET is_completed = 1 , update_user_id = 1
-WHERE created_at BETWEEN '2020/4/26 09:00' AND '2020/4/26 11:30'
+WHERE is_deleted = 0 AND created_at BETWEEN '2020/4/26 09:00' AND '2020/4/26 11:30';
 COMMIT;
--- db/db-challenge3/db-012/db-012.sql
+++ db/db-challenge3/db-012/db-012.sql
@@ -1,5 +1,9 @@
 BEGIN;
 UPDATE `chat_rooms`
 SET can_sent_file = 0 , update_user_id = 1
-WHERE create_user_id <> 1 AND is_deleted = 0
+WHERE id NOT IN (
+    SELECT room_id
+    FROM `rooms_join_users`
+    WHERE user_id = 1)
+AND is_deleted = 0;
 COMMIT;
